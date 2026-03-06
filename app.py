5c3fa91c11623c73e76a8a19f1d4653b8236c06a
Ana <ana.rjota@gmail.com>
2026-03-06 12:27:18 -0300

    removendo venv do versionamento e adicionando gitignore

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+GITHUB_USER = "anajota"
+
+def get_repos():
+    url = f"https://api.github.com/users/{GITHUB_USER}/repos"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    
+    return []
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    repos = get_repos()
+
+    return render_template("index.html", repos=repos)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
